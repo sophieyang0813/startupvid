b95b21722289e4a7b27cd8b24bfb6270e4dd74db
sophieyang0813@gmail.com <sophieyang@Sophies-MacBook-Pro.local>
2017-11-17 13:06:06 +0800

    now functioning sign in/sign up page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
       @user = User.find_by(email: params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      redirect_to user_posts_path(current_user)
+      redirect_to users_path
     else
       flash[:warning] = "Invalid username or password"
       redirect_to new_session_path
